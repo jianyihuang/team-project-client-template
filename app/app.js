7c539eb55f0fd746171f9ec03efd866dbf17bc66
Jucong He <hejucong1995@gmail.com>
2016-11-06 23:37:27 -0500

    add router to webpack and app.js

--- app/app.js
+++ app/app.js
@@ -1,8 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CategoryBox from './components/categorybox';
+import { IndexRoute,Router,Route,browserHistory } from 'react-router';
 
-ReactDOM.render(
-  <CategoryBox />,
-  document.getElementById('categories-box')
+class ProfilePage extends React.Component {
+  render() {
+    return (
+      <p>This is the profile page for a user
+         with ID {this.props.params.id}.</p>
+    );
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>{this.props.children}</div>
+    )
+  }
+}
+
+class CategoryBoxPage extends React.Component {
+  render() {
+    return <CategoryBox />;
+  }
+}
+
+ReactDOM.render((
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+      {/* Show the Feed at / */}
+      <IndexRoute component={CategoryBoxPage} />
+      <Route path="profile/:id" component={ProfilePage} />
+      </Route>
+    </Router>
+  ),document.getElementById('categories-box')
 );
--- app/components/categorybox.js
+++ app/components/categorybox.js
@@ -1,4 +1,5 @@
 import React from 'react';
+
 export default class CategoryBox extends React.Component {
   render() {
     return(
