2de77806277274ef52d5c29d6166f6867dc5dacf
Jucong He <hejucong1995@gmail.com>
2016-11-06 23:04:20 -0500

    change index page to academic page change category box to react components

--- app/app.js
+++ app/app.js
@@ -1,1 +1,8 @@
-/* Empty for now. :) */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CategoryBox from './components/categorybox';
+
+ReactDOM.render(
+  <CategoryBox />,
+  document.getElementById('categories-box')
+);
---
+++ app/components/categorybox.js
@@ -0,0 +1,61 @@
+import React from 'react';
+export default class CategoryBox extends React.Component {
+  render() {
+    return(
+      <div>
+        <div className="row">
+          <div className="col-md-4">
+            <div className="panel panel-default">
+              <div className="panel-body">
+                <h5 className="text-center">Computer Science</h5>
+                  <a href="#"><img src="img/cmpSci.jpeg"/></a>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="panel panel-default">
+              <div className="panel-body">
+                <h5 className="text-center">Math</h5>
+                <a href="#"><img src="img/math.jpg"/></a>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="panel panel-default">
+              <div className="panel-body">
+                <h5 className="text-center">Physics</h5>
+                <a href="#"><img src="img/physics.jpeg"/></a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-4">
+            <div className="panel panel-default">
+              <div className="panel-body">
+                <h5 className="text-center">Music</h5>
+                <a href="#"><img src="img/music.jpg"/></a>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="panel panel-default">
+              <div className="panel-body">
+                <h5 className="text-center">Histroy</h5>
+                <a href="#"><img src="img/history.jpg"/></a>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="panel panel-default">
+              <div className="panel-body">
+                <h5 className="text-center">English</h5>
+                <a href="#"><img src="img/english.jpg"/></a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
