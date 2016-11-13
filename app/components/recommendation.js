019a0d477e4cf3026d097fc7d3df402f0359f180
Jucong He <hejucong1995@gmail.com>
2016-11-12 21:33:25 -0500

    break detail page into two components

---
+++ app/components/recommendation.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class Recommendation extends React.Component {
+  render() {
+    return(
+      <a href="#" className="list-group-item list-group-item-action">
+        <h5 className="list-group-item-heading">Help with Cal1 please</h5>
+        <p className="list-group-item-text">Donec id elit non mi porta
+           gravida at eget metus. Maecenas sed diam eget risus varius
+           blandit.
+        </p>
+      </a>
+    );
+  }
+}
