a733d3acfe80f52fec1831f19fc9c2a88dd56422
xin71 <lx199571@gmail.com>
2016-11-15 16:09:13 -0500

    schedule success

---
+++ app/components/schedulebox.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+export class Schedulebox extends React.Component {
+	constructor(props){
+		super(props);
+		console.log("we see schedule Box");
+	}
+	render() {
+		return (
+			<div className="row">
+				<div className="col-md-3 text-center">
+			<div className="panel panel-default whole-tab">
+				<div className="panel-body">
+					<span className = "tab-tittle">{this.props.firstName}</span>
+							<br /><span className= "taken-by">{this.props.postDate}</span><br />
+							<span className = "tab-subject">{this.props.serviceContent}</span>
+							<br /><span className = "regular-text">{this.props.startTime} - {this.props.endTime}</span><br />
+							<span className = "taken-by">APPOINTMENT WITH</span>
+							<br /><span className = "taken-by">{this.props.party}</span><br />
+							<hr />
+							<button type="button" className="btn btn-primary completed-button">
+								{this.props.completed}
+							</button>
+							</div>
+							</div>
+						</div>
+					</div>
+			);
+	}
+}
