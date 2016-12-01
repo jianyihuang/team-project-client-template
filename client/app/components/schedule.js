a4788cb75c42c7665eaa592e39a5b468b59b7dd8
xin71 <lx199571@gmail.com>
2016-11-30 23:32:35 -0500

    start schedule part

--- client/app/components/schedule.js
+++ client/app/components/schedule.js
@@ -42,6 +42,79 @@ export default class Schedule extends React.Component {
           <p><button type='button' onClick={resetDatabase}> Reset Database</button></p>
         </div>
 
+        <div className="col-xs-3"/>
+        <div className="col-xs-6">
+          <div className="panel panel-default">
+            <div className= "panel-color">
+              <div className="panel-body">
+                <font size="5">Add an appointment </font>
+                <div className="row">
+                  <div className= "col-xs-3">
+                    <strong>Your Name</strong>
+                  </div>
+                  <div className= "col-xs-5">
+                    <div className="input-style">
+                      <input type="text" className="form-control" value={this.state.username} onChange={this.handleUsernameChange}/>
+                    </div>
+                  </div>
+                </div>
+                <hr />
+                <div className="row">
+                  <div className= "col-xs-3">
+                    <strong>Date(e.g. 10/6/2016)</strong>
+                  </div>
+                  <div className= "col-xs-5">
+                    <div className="input-style">
+                      <input type="text" className="form-control" value={this.state.email} onChange={this.handleEmailChange} />
+                    </div>
+                  </div>
+                </div>
+                <hr />
+                <div className="row">
+                  <div className= "col-xs-3">
+                    <strong>Service(e.g. CS121)</strong>
+                  </div>
+                  <div className= "col-xs-5">
+                    <div className="input-style">
+                      <input type="text" className="form-control" value={this.state.password} onChange={this.handlePasswordChange} />
+                    </div>
+                  </div>
+                </div>
+                <hr />
+              <div className="row">
+                    <div className= "col-xs-3">
+                      <strong>Time(e.g. 9AM-12AM)</strong>
+                    </div>
+                    <div className= "col-xs-5">
+                      <div className="input-style">
+                        <input type="text" className="form-control" value={this.state.password} onChange={this.handlePasswordChange} />
+                      </div>
+                    </div>
+                  </div>
+                  <hr />
+                  <div className="row">
+              <div className= "col-xs-3">
+              <strong>Subscriber</strong>
+                </div>
+              <div className= "col-xs-5">
+            <div className="input-style">
+                <input type="text" className="form-control" value={this.state.password} onChange={this.handlePasswordChange} />
+            </div>
+              </div>
+            </div>
+          <hr />
+                <div className="text-center">
+                  <div className="btn-group" role="group">
+                    <button className="button-style" onClick={this.handleSave} >
+                      <span className="glyphicon glyphicon-check"></span>
+                      Add to Schedule
+                    </button>
+                  </div>
+              </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
